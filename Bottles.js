f7035b0ce9eedfbca8fedf23b4eeb42f5f9b1c24
bobcataryan <83252651+bobcataryan@users.noreply.github.com>
2022-03-03 19:42:22 -0600

    Add files via upload

---
+++ Bottles.js
@@ -0,0 +1,52 @@
+
+objectdetector="";
+status="";
+img="";
+var objects="";
+
+function preload(){
+    img=loadImage('water bottle.jpeg');
+}
+
+function draw(){
+    image(img,0,0,400,500)
+}
+
+
+
+function setup(){
+    canvas=createCanvas(450,500);
+    canvas.center();
+    objectdetector=ml5.objectDetector('cocossd',modelloaded);
+    document.getElementById("status").innerHTML="Satus : Detecting Objects";
+}
+
+function modelloaded(){
+    console.log("Model has loaded");
+    status =true;
+    objectdetector.detect(img,gotResults);
+}
+
+function gotResults(error,results){
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
+}
+
+
+function draw(){
+    image(img,0,0,400,500)
+    k=objects.length;
+    for(i=0;i<k;i++){
+        stroke('#fc1100');
+        text(objects[i].label,objects[i].x+15,objects[i].y+15);
+        noFill();
+        stroke('#fc1100')
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        document.getElementById("status").innerHTML="Status: Objects Detected";
+    }
+}
---
+++ Desk.js
@@ -0,0 +1,53 @@
+
+objectdetector="";
+status="";
+img="";
+var objects="";
+
+function preload(){
+    img=loadImage('Desk.jpg');
+}
+
+function draw(){
+    image(img,0,0,400,500)
+}
+
+
+
+function setup(){
+    canvas=createCanvas(450,500);
+    canvas.position(700,200);
+
+    objectdetector=ml5.objectDetector('cocossd',modelloaded);
+    document.getElementById("status").innerHTML="Satus : Detecting Objects";
+
+}
+
+function modelloaded(){
+    console.log("Model has loaded");
+    status =true;
+    objectdetector.detect(img,gotResults);
+}
+
+function gotResults(error,results){
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
+}
+
+
+function draw(){
+    image(img,0,0,400,500)
+    k=objects.length;
+    for(i=0;i<k;i++){
+        stroke('#fc1100');
+        text(objects[i].label,objects[i].x+15,objects[i].y+15);
+        noFill();
+        stroke('#fc1100')
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+    }
+}
